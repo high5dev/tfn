d8dbaa80a68131f558a7b2c0215a74270f471969
high5dev <high5dev621@hotmail.com>
2021-06-13 14:58:23 +0100

    Getting ready

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RolesAndPermissionsSeeder::class);
         $this->call(UsersTableSeeder::class);
 
         // assign admin role to admin user
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header text-center">The Secret Portaly</div>
+                <div class="card-header text-center">The Secret Portal</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
