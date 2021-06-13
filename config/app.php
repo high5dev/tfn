d61c37283d097d22c3053b9c12b26e1d69664008
high5dev <high5dev621@hotmail.com>
2021-06-13 16:57:11 +0100

    Getting ready

--- config/app.php
+++ config/app.php
@@ -2,6 +2,8 @@
 
 return [
 
+    'dictionary' => env('DICTIONARY', 'dictionary.txt'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Name
