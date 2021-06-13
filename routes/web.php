6ae628870799eb6321cb990265a7615e9cd78209
high5dev <high5dev621@hotmail.com>
2021-06-13 16:35:09 +0100

    Getting ready

--- routes/web.php
+++ routes/web.php
@@ -38,14 +38,14 @@ Route::get('logout', 'LoggedInController@logout');
  */
 // public routes
 Route::get('/', 'PagesController@index')->name('index');
-Route::get('/home', 'LoggedInController@home')->name('home');
+
 // pages for logged in users
 Route::group([
     'middleware' => 'auth'
 ], function () {
 
     // User: homepage and profile
-    //Route::get('/home', 'LoggedInController@home')->name('home');
+    Route::get('/home', 'LoggedInController@home')->name('home');
     Route::get('/profile', 'ProfileController@show');
     Route::post('/profile', 'ProfileController@update');
 
@@ -58,7 +58,3 @@ Route::group([
     Route::delete('/admin/users/{id}', 'Admin\AdminUserController@destroy');
 
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
