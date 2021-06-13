2372c4b6125697ff9be54db1a3b67b6d34045434
high5dev <high5dev621@hotmail.com>
2021-06-13 14:59:52 +0100

    Getting ready

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        // create permissions
+        Permission::create(['name' => 'login']);
+
+        Permission::create(['name' => 'view users']);
+        Permission::create(['name' => 'create users']);
+        Permission::create(['name' => 'update users']);
+        Permission::create(['name' => 'delete users']);
+
+        Permission::create(['name' => 'view logs']);
+
+        // create the admin role
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo(Permission::all());
+
+        // create the basic user role
+        $role = Role::create(['name' => 'user']);
+        $role->givePermissionTo('login');
+
+    }
+}
