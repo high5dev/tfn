0f503bfa671ea61da773cedb46432742a1a4f037
high5dev <high5dev621@hotmail.com>
2021-06-13 17:33:02 +0100

    Getting ready

---
+++ resources/views/log/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('content')
+
+@include('layouts.flash_message')
+
+  <h3>List Log Entries</h3>
+
+      <table class="table table-striped">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">Timestamp</th>
+            <th scope="col">Title</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($logs as $log)
+          <tr>
+            <td>{{ $log->created_at }}</td>
+            <td><a href="/logs/{{ $log->id }}">{{ $log->title }}</a></td>
+          </tr>
+        @endforeach
+        </tbody>
+      </table>
+
+@endsection
---
+++ resources/views/log/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('content')
+
+@include('layouts.flash_message')
+
+  <h3>View a log entry</h3>
+
+      <form method="post" action="#">
+
+        <fieldset>
+
+		    <legend>Log Details</legend>
+
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="title">Title :</label>
+                <div class="col-sm-10">
+                    <input type="text" id="title" name="title" class="form-control" readonly value="{{ $log->title }}">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="created_at">Timestamp :</label>
+                <div class="col-sm-10">
+                    <input type="text" id="created_at" name="created_at" class="form-control" readonly value="{{ $log->created_at }}">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="content">Content :</label>
+                <div class="col-sm-10">
+                    <textarea class="form-control" rows="10">{{ $log->content }}</textarea>
+                </div>
+            </div>
+
+        </fieldset>
+
+      </form>
+
+      <a href="/logs">Back to list</a>
+
+@endsection
