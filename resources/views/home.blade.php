ba4296a8bc5cafbfd780ec12baf9633bb33b944e
high5dev <high5dev621@hotmail.com>
2021-06-13 16:29:09 +0100

    Getting ready

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     @include('layouts.flash_message')
 
     <div class="jumbotron">
-        Hello {{ $name }},x
+        Hello {{ $name }},
         @if (strlen($lastLoggedIn))
             you last logged in {{ $lastLoggedIn }}
         @else
