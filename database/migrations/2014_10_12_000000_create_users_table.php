96f6114d4c823c1ec10675cbcc166e58cafc6897
high5dev <high5dev621@hotmail.com>
2021-06-13 17:07:23 +0100

    Getting ready

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('last_login_ip', 45)->default('');
+            $table->datetime('last_login_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
