160ffa27c4aba8c2bf4f38d152b72903a8d623bb
high5dev <high5dev621@hotmail.com>
2021-06-13 17:27:24 +0100

    Getting ready

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+@section('content')
+
+    @include('layouts.flash_message')
+
+    <h3>List Users</h3>
+
+    <a href="/admin/users/create">Create a new user</a>
+
+    <table class="table table-striped">
+        <thead class="thead-light">
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <td>
+                    {{ $user->name }}
+                </td>
+                <td>
+                    {{ $user->email }}
+                </td>
+                <td>
+                    <div class="row">
+                        <div class="span6">
+                            <form method="get" action="/admin/users/{{ $user->id }}">
+                                @csrf
+                                <button class='btn btn-default' type="submit" alt="Edit">
+                                    <span class="fa fa-edit" aria-hidden="true"></span>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="span6">
+                            <form method="post" action="/admin/users/{{ $user->id }}">
+                                @csrf
+                                @method("DELETE")
+                                <button class='btn btn-default' type="submit" alt="Delete"
+                                        onclick="return okCancel('Are you sure you want to delete this user?')">
+                                    <span class="fa fa-trash" aria-hidden="true"></span>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+@endsection
