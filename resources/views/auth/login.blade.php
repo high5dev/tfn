ac41f4cb735b79fb0f01300d5ebdd472e7ebf9ac
high5dev <high5dev621@hotmail.com>
2021-06-14 17:44:09 +0100

    Searching

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,8 +12,7 @@
                             @csrf
 
                             <div class="form-group row">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
@@ -29,9 +28,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
+                                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                            class="form-control @error('password') is-invalid @enderror" name="password"
@@ -50,9 +47,8 @@
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember"
                                                id="remember" {{ old('remember') ? 'checked' : '' }}>
-
                                         <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
+                                            Remember Me
                                         </label>
                                     </div>
                                 </div>
@@ -61,12 +57,11 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
+                                        Login
                                     </button>
-
                                     @if (Route::has('password.request'))
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
+                                            Forgot Your Password?
                                         </a>
                                     @endif
                                 </div>
