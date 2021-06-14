b79093e5347ee3383f2e7921e07ea5dfe465ad1b
high5dev <high5dev621@hotmail.com>
2021-06-14 19:49:59 +0100

    Searching

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,11 +6,11 @@
 
     <h3>Scan Posts</h3>
 
+    <div>Scan by post ID:</div>
     <form method="post" action="/posts/byid" class="form-inline">
         @csrf()
 
         <div class="form-group">
-            <label class="" for="postid">Start Post ID:</label>
             <input type="text" id="postid" name="postid" class="form-control" value="{{ old('postid') }}"
                    placeholder="Post ID" {{ $errors->has('postid') ? ' autofocus' : '' }} required>
             @if ($errors->has('postid'))
@@ -19,7 +19,6 @@
         </div>
 
         <div class="form-group">
-            <label class="sr-only" for="postid">Type:</label>
             <select id="type" name="type" class="form-control">
                 <option value="b">Both</option>
                 <option value="o">OFFERs</option>
@@ -34,11 +33,11 @@
 
     </form>
 
+    <div>Scan by date & time:</div>
     <form method="post" action="/posts/bytime" class="form-inline">
         @csrf()
 
         <div class="form-group">
-            <label class="" for="date">Start Date:</label>
             <input type="text" id="date" name="date" class="form-control" value="{{ old('date') }}"
                    placeholder="YYYY-MM-DD" {{ $errors->has('date') ? ' autofocus' : '' }} required>
             @if ($errors->has('date'))
@@ -47,7 +46,6 @@
         </div>
 
         <div class="form-group">
-            <label class="" for="date">Start Time:</label>
             <input type="text" id="time" name="time" class="form-control" value="{{ old('time') }}"
                    placeholder="HH:MM" {{ $errors->has('time') ? ' autofocus' : '' }} required>
             @if ($errors->has('time'))
@@ -56,7 +54,6 @@
         </div>
 
         <div class="form-group">
-            <label class="sr-only" for="postid">Type:</label>
             <select id="type" name="type" class="form-control">
                 <option value="b">Both</option>
                 <option value="o">OFFERs</option>
