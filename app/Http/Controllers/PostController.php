a2ba50c325f22c8773f05bd839990b5a50d428e4
high5dev <high5dev621@hotmail.com>
2021-06-14 17:25:03 +0100

    Searching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,8 +33,8 @@ class PostController extends Controller
 
         if ($post) {
             $post->delete();
-            return view('/home')->with('success', 'Successfully removed the post');
+            return view('search.index')->with('success', 'Successfully removed the post');
         }
-        return view('/home')->with('error', 'Unable to find that post!');
+        return view('search.index')->with('error', 'Unable to find that post!');
     }
 }
