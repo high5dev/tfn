e2afb401389454c9d5fd86c04af01ecf0fb78fe5
high5dev <high5dev621@hotmail.com>
2021-06-14 19:47:30 +0100

    Searching

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,13 +6,13 @@
 
     <h3>Scan Posts</h3>
 
-    <form method="post" action="/posts/list" class="form-inline">
+    <form method="post" action="/posts/byid" class="form-inline">
         @csrf()
 
         <div class="form-group">
-            <label class="sr-only" for="postid">Start Post ID:</label>
+            <label class="" for="postid">Start Post ID:</label>
             <input type="text" id="postid" name="postid" class="form-control" value="{{ old('postid') }}"
-                   {{ $errors->has('postid') ? ' autofocus' : '' }} required>
+                   placeholder="Post ID" {{ $errors->has('postid') ? ' autofocus' : '' }} required>
             @if ($errors->has('postid'))
                 <div class="alert alert-warning">{{ $errors->first('postid') }}</div>
             @endif
@@ -29,7 +29,44 @@
 
         <div class="form-group">
             <label class="sr-only" for="submit">&nbsp;</label>
-            <input type="submit" id="submit" name="submit" value="Create">
+            <input type="submit" id="submit" name="submit" value="Go">
+        </div>
+
+    </form>
+
+    <form method="post" action="/posts/bytime" class="form-inline">
+        @csrf()
+
+        <div class="form-group">
+            <label class="" for="date">Start Date:</label>
+            <input type="text" id="date" name="date" class="form-control" value="{{ old('date') }}"
+                   placeholder="YYYY-MM-DD" {{ $errors->has('date') ? ' autofocus' : '' }} required>
+            @if ($errors->has('date'))
+                <div class="alert alert-warning">{{ $errors->first('date') }}</div>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label class="" for="date">Start Time:</label>
+            <input type="text" id="time" name="time" class="form-control" value="{{ old('time') }}"
+                   placeholder="HH:MM" {{ $errors->has('time') ? ' autofocus' : '' }} required>
+            @if ($errors->has('time'))
+                <div class="alert alert-warning">{{ $errors->first('time') }}</div>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label class="sr-only" for="postid">Type:</label>
+            <select id="type" name="type" class="form-control">
+                <option value="b">Both</option>
+                <option value="o">OFFERs</option>
+                <option value="w">WANTEDs</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label class="sr-only" for="submit">&nbsp;</label>
+            <input type="submit" id="submit" name="submit" value="Go">
         </div>
 
     </form>
