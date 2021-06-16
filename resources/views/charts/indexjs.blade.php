096bd16d2b368d747e8e9bf407aa73279c0599df
high5dev <high5dev621@hotmail.com>
2021-06-16 20:36:45 +0100

    Searching

--- resources/views/charts/indexjs.blade.php
+++ resources/views/charts/indexjs.blade.php
@@ -20,7 +20,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script>
         var chartData = {
-                labels: {{ json_encode($dates)  }},
+                labels: {!! json_encode($dates)  !!},
             datasets: [{
                 label: 'OFFERS',
                 backgroundColor: "green",
