2c0527d59572849d7a1f6bd71b7ecad2ff049154
high5dev <high5dev621@hotmail.com>
2021-06-16 20:40:46 +0100

    Searching

--- resources/views/charts/indexjs.blade.php
+++ resources/views/charts/indexjs.blade.php
@@ -20,24 +20,25 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script>
         var chartData = {
-                labels: {!! json_encode($dates)  !!},
-            datasets: [{
-                label: 'OFFERS',
-                backgroundColor: "green",
-                data: {{ $offers }}
-            }],
-            datasets: [{
-                label: 'WANTEDS',
-                backgroundColor: "blue",
-                data: {{ $wanteds }}
-            }],
-            datasets: [{
-                label: 'ZAPS',
-                backgroundColor: "red",
-                data: {{ $zaps }}
-            }]
-        }
-        ;
+            labels: {!! json_encode($dates)  !!},
+            datasets: [
+                {
+                    label: 'OFFERS',
+                    backgroundColor: "green",
+                    data: {{ $offers }}
+                },
+                {
+                    label: 'WANTEDS',
+                    backgroundColor: "blue",
+                    data: {{ $wanteds }}
+                },
+                {
+                    label: 'ZAPS',
+                    backgroundColor: "red",
+                    data: {{ $zaps }}
+                }
+            ]
+        };
 
         window.onload = function () {
             var ctx = document.getElementById("canvas").getContext("2d");
