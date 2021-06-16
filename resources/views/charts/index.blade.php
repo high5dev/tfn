21d76632bfe6f0ed677b8923187d08d685b22729
high5dev <high5dev621@hotmail.com>
2021-06-16 20:53:18 +0100

    Searching

--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -58,6 +58,23 @@
                             display: true,
                             text: 'Last week\'s posts'
                         }
+                    },
+                    scales: {
+                        y: {
+                            type: 'linear',
+                            display: true,
+                            position: 'left',
+                        },
+                        y1: {
+                            type: 'linear',
+                            display: true,
+                            position: 'right',
+
+                            // grid line settings
+                            grid: {
+                                drawOnChartArea: false, // only want the grid lines for one axis to show up
+                            },
+                        },
                     }
                 },
             });
