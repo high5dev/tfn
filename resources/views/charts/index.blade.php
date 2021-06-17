90ee959882017b379ce88497c1fa6c72c2e0a06f
high5dev <high5dev621@hotmail.com>
2021-06-17 20:14:19 +0100

    Searching

--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -73,9 +73,18 @@
                             text: 'Posts',
                             type: 'linear',
                             position: 'left',
+                            display: true,
                             title: {
                                 display: true,
-                                text: 'Posts'
+                                text: 'Posts',
+                                color: '#191',
+                                font: {
+                                    family: 'Times',
+                                    size: 20,
+                                    style: 'normal',
+                                    lineHeight: 1.2
+                                },
+                                padding: {top: 30, left: 0, right: 0, bottom: 0}
                             },
                             ticks: {
                                 beginAtZero: true,
@@ -85,9 +94,18 @@
                             text: 'Zaps',
                             type: 'linear',
                             position: 'right',
+                            display: true,
                             title: {
                                 display: true,
-                                text: 'Zaps'
+                                text: 'Zaps',
+                                color: '#191',
+                                font: {
+                                    family: 'Times',
+                                    size: 20,
+                                    style: 'normal',
+                                    lineHeight: 1.2
+                                },
+                                padding: {top: 30, left: 0, right: 0, bottom: 0}
                             },
                             ticks: {
                                 beginAtZero: true,
