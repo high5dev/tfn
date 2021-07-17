edfbcb5f794eb77b78502592931645cb45984851
high5dev <high5dev621@hotmail.com>
2021-07-17 17:35:15 +0100

    Scanning

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,22 +16,22 @@
     </div>
 
     <div>Scan from midnight:</div>
-    <form method="get" action="/posts/list" class="form-inline">
+    <form method="get" action="/posts/list" class="form-inline row">
         @csrf()
 
-        <div class="form-group">
-            <select id="type" name="type" class="form-control">
+        <div class="form-group col-sm-4">
+            <select id="type" name="type" class="form-control>
                 <option value="b">Both</option>
                 <option value="o">OFFERs</option>
                 <option value="w">WANTEDs</option>
             </select>
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-4">
             Scanning: <input type="checkbox" id="scanning" name="scanning" value="1">
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-4">
             <label class="sr-only" for="submit">&nbsp;</label>
             <input type="submit" id="submit" name="submit" value="Go">
         </div>
@@ -41,15 +41,15 @@
     </form>
 
     <div>Scan by post ID:</div>
-    <form method="get" action="/posts/list" class="form-inline">
+    <form method="get" action="/posts/list" class="form-inline row">
         @csrf()
 
-        <div class="form-group">
+        <div class="form-group col-sm-3">
             <input type="text" id="postid" name="postid" class="form-control" value="{{ old('postid') }}"
                    placeholder="Post ID" {{ $errors->has('postid') ? ' autofocus' : '' }} required>
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-3">
             <select id="type" name="type" class="form-control">
                 <option value="b">Both</option>
                 <option value="o">OFFERs</option>
@@ -57,11 +57,11 @@
             </select>
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-3">
             Scanning: <input type="checkbox" id="scanning" name="scanning" value="1">
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-3">
             <label class="sr-only" for="submit">&nbsp;</label>
             <input type="submit" id="submit" name="submit" value="Go">
         </div>
@@ -71,20 +71,20 @@
     </form>
 
     <div>Scan by date & time:</div>
-    <form method="get" action="/posts/list" class="form-inline">
+    <form method="get" action="/posts/list" class="form-inline row">
         @csrf()
 
-        <div class="form-group">
+        <div class="form-group col-sm-4">
             <input type="text" id="date" name="date" class="form-control" value="{{ old('date') }}"
                    placeholder="YYYY-MM-DD" {{ $errors->has('date') ? ' autofocus' : '' }} required>
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-2">
             <input type="text" id="time" name="time" class="form-control" value="{{ old('time') }}"
                    placeholder="HH:MM" {{ $errors->has('time') ? ' autofocus' : '' }} required>
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-2">
             <select id="type" name="type" class="form-control">
                 <option value="b">Both</option>
                 <option value="o">OFFERs</option>
@@ -92,11 +92,11 @@
             </select>
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-2">
             Scanning: <input type="checkbox" id="scanning" name="scanning" value="1">
         </div>
 
-        <div class="form-group">
+        <div class="form-group col-sm-2">
             <label class="sr-only" for="submit">&nbsp;</label>
             <input type="submit" id="submit" name="submit" value="Go">
         </div>
