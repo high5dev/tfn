fe4bee2010f8355940666f3b8e40dc37d2c1f47a
high5dev <high5dev621@hotmail.com>
2021-07-18 20:44:12 +0100

    bug fix

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // we don't need timestamps in this model as we already have start & finish times
+    public $timestamps = false;
 }
