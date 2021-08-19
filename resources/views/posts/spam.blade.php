659810f41ff0e5d7441bdd454ec875ba87fe5a60
high5dev <high5dev621@hotmail.com>
2021-08-19 09:01:37 +0100

    bug fix

--- resources/views/posts/spam.blade.php
+++ resources/views/posts/spam.blade.php
@@ -55,13 +55,13 @@
                         @endif
                     </td>
                     <td>
-                        <form method="post" action="/posts/notspam/{{ $post->id }}" class="input-group pull-left">
+                        <form method="post" action="/posts/notspam/{{ $post->id }}" class="form-inline">
                             @csrf()
                             <button class="btn btn-sm btn-outline-secondary" type="submit">
                                 <i class="fas fa-toilet-paper-slash"></i>
                             </button>
                         </form>
-                        <form method="post" action="/post/{{ $post->id }}" class="input-group pull-right"
+                        <form method="post" action="/post/{{ $post->id }}" class="form-inline"
                               onsubmit="return confirm('Are you sure you want to remove this post?');">
                             @method('DELETE')
                             @csrf
