66a74d3471388db9df11c8a47f9d97cd62380434
high5dev <high5dev621@hotmail.com>
2021-09-18 12:31:14 +0100

    User's Efficiency Chart

--- resources/views/charts/users.blade.php
+++ resources/views/charts/users.blade.php
@@ -10,7 +10,7 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">User Efficiency</div>
                     <div class="panel-body">
-                        <canvas id="canvas" height="280" width="600"></canvas>
+                        <canvas id="bar-chart" width="800" height="450"></canvas>
                     </div>
                 </div>
             </div>
@@ -19,56 +19,26 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script>
-        const labels = Utils.months({count: 7});
-        const data = {
-            labels: labels,
-            datasets: [{
-                label: 'User Efficiency',
-                dataata: [65, 59, 80, 81, 56, 55, 40],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 205, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(201, 203, 207, 0.2)'
-                ],
-                borderColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 205, 86)',
-                    'rgb(75, 192, 192)',
-                    'rgb(54, 162, 235)',
-                    'rgb(153, 102, 255)',
-                    'rgb(201, 203, 207)'
-                ],
-                borderWidth: 1
-            }]
-        };
-
-        window.onload = function () {
-            new Chart(document.getElementById("canvas"), {
-                type: 'bar',
-                data: {
-                    labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-                    datasets: [
-                        {
-                            label: "Population (millions)",
-                            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-                            data: [2478,5267,734,784,433]
-                        }
-                    ]
-                },
-                options: {
-                    legend: { display: false },
-                    title: {
-                        display: true,
-                        text: 'Predicted world population (millions) in 2050'
+        new Chart(document.getElementById("bar-chart"), {
+            type: 'bar',
+            data: {
+                labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+                datasets: [
+                    {
+                        label: "Population (millions)",
+                        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                        data: [2478,5267,734,784,433]
                     }
+                ]
+            },
+            options: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: 'Predicted world population (millions) in 2050'
                 }
-            });
-        };
+            }
+        });
     </script>
 
 @endsection
