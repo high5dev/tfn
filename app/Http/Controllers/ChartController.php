347c048f878db1050c15154af75b1288b5f21772
high5dev <high5dev621@hotmail.com>
2021-09-18 13:31:28 +0100

    User's Efficiency Chart

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -56,7 +56,7 @@ class ChartController extends Controller
      */
     public function users()
     {
-        $colours = '["#ffffff", "#aa0000","#00ff00","#0000ff","#c45850","#aaaaaa"]';
+        $colours = '["#000000", "#aa0000","#00ff00","#0000ff","#c45850","#aaaaaa"]';
         $users = '["Ben", "Chris", "Debbie", "Dennis", "Pat", "Valentina"]';
         $efficiency = "[2478, 5267, 734, 784, 433, 4444]";
         return view('charts.users', compact('colours','users', 'efficiency'));
