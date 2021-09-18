78f9b10244a080246fc1e0f3775d4232c28f3459
high5dev <high5dev621@hotmail.com>
2021-09-18 12:12:12 +0100

    User's Efficiency Chart

--- resources/views/charts/users.blade.php
+++ resources/views/charts/users.blade.php
@@ -19,27 +19,32 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script>
-        var chartData = {
-            datasets: [
-                {
-                    label: 'User A',
-                    borderColor: "green",
-                    backgroundColor: 'transparent',
-                    data: 5,
-                },
-                {
-                    label: 'User B',
-                    borderColor: "blue",
-                    backgroundColor: 'transparent',
-                    data: 15,
-                },
-                {
-                    label: 'User C',
-                    borderColor: "red",
-                    backgroundColor: 'transparent',
-                    data: 10,
-                }
-            ]
+        const labels = Utils.months({count: 7});
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'User Efficiency',
+                chartData: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+            }]
         };
 
         window.onload = function () {
@@ -48,21 +53,11 @@
                 type: 'bar',
                 data: chartData,
                 options: {
-                    responsive: true,
-                    interaction: {
-                        mode: 'index',
-                        intersect: false,
-                    },
-                    stacked: false,
-                    plugins: {
-                        legend: {
-                            position: 'top',
-                        },
-                        title: {
-                            display: true,
-                            text: 'User Efficiency'
+                    scales: {
+                        y: {
+                            beginAtZero: true
                         }
-                    },
+                    }
                 },
             });
         };
