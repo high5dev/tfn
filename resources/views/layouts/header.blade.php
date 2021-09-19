65958ecccbba23c1a1ae35ca8ae1b64765b1ccf5
high5dev <high5dev621@hotmail.com>
2021-09-19 19:37:07 +0100

    Groups

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,14 +12,14 @@
     <link href="/css/select2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/dataTables.bootstrap5.min.css">
 
-    <script src="/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="/js/jquery-3.6.0.min.js"></script>
     <script src="/js/popper.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <script src="/js/select2.full.min.js"></script>
     <script src="/js/all.min.js"></script>
     <script src="/js/scripts.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="/js/dataTables.bootstrap5.min.js"></script>
 
 </head>
