496bf3c608305a6f2c2e4a4fb09c1fb3f3027dc2
high5dev <high5dev621@hotmail.com>
2021-09-20 09:43:45 +0100

    Groups

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,7 @@
     <title>The Secret Portal</title>
 
     <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="stylesheet" href="/bootstrap-5.0.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/bootstrap-4.6.0-dist/css/bootstrap.css">
     <link rel="stylesheet" href="/css/select2.min.css">
     <link rel="stylesheet" href="/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css"/>
@@ -16,7 +16,7 @@
     <link rel="stylesheet" type="text/css" href="/dataTables/FixedHeader-3.1.9/css/fixedHeader.bootstrap4.min.css"/>
 
     <script src="/js/jquery-3.6.0.min.js"></script>
-    <script src="/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
+    <script src="/bootstrap-4.6.0-dist/js/bootstrap.js"></script>
     <script type="text/javascript" src="/dataTables/DataTables-1.11.2/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="/dataTables/DataTables-1.11.2/js/dataTables.bootstrap4.min.js"></script>
     <script type="text/javascript" src="/dataTables/FixedHeader-3.1.9/js/dataTables.fixedHeader.min.js"></script>
