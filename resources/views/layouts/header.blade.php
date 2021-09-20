e7234e5f8e526014188d7a64462840f485b5ff0e
high5dev <high5dev621@hotmail.com>
2021-09-20 09:38:50 +0100

    Groups

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,12 +12,14 @@
     <link rel="stylesheet" href="/css/select2.min.css">
     <link rel="stylesheet" href="/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css"/>
-    <link rel="stylesheet" type="text/css" href="/dataTables/DataTables-1.11.2/css/dataTables.bootstrap5.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/dataTables/DataTables-1.11.2/css/dataTables.bootstrap4.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/dataTables/FixedHeader-3.1.9/css/fixedHeader.bootstrap4.min.css"/>
 
     <script src="/js/jquery-3.6.0.min.js"></script>
     <script src="/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
     <script type="text/javascript" src="/dataTables/DataTables-1.11.2/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="/dataTables/DataTables-1.11.2/js/dataTables.bootstrap5.min.js"></script>
+    <script type="text/javascript" src="/dataTables/DataTables-1.11.2/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="/dataTables/FixedHeader-3.1.9/js/dataTables.fixedHeader.min.js"></script>
     <script src="/js/popper.min.js"></script>
     <script src="/js/select2.full.min.js"></script>
     <script src="/js/all.min.js"></script>
