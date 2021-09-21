fc41bc8c345c87e91692a052d337efe7b0d8166a
high5dev <high5dev621@hotmail.com>
2021-09-21 13:54:58 +0100

    Bug fix

--- resources/views/layouts/flash_message.blade.php
+++ resources/views/layouts/flash_message.blade.php
@@ -1,27 +1,27 @@
 @if ($message = Session::get('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>{{ $message }}</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert">×</button>
     </div>
 @endif
 
 @if ($message = Session::get('error'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <strong>{{ $message }}</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert">×</button>
     </div>
 @endif
 
 @if ($message = Session::get('warning'))
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>{{ $message }}</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert">×</button>
     </div>
 @endif
 
 @if ($message = Session::get('info'))
     <div class="alert alert-info alert-dismissible fade show" role="alert">
         <strong>{{ $message }}</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert">×</button>
     </div>
 @endif
