474641360d696c7b9837ef9e9ff666a28c5a4c76
high5dev <high5dev621@hotmail.com>
2021-11-07 17:16:13 +0000

    permissions update

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -21,16 +21,24 @@ class RolesAndPermissionsSeeder extends Seeder
         // create permissions
         Permission::create(['name' => 'login']);
 
+        Permission::create(['name' => 'admin view graphs']);
+
+        Permission::create(['name' => 'view groups']);
+        Permission::create(['name' => 'create groups']);
+        Permission::create(['name' => 'update groups']);
+        Permission::create(['name' => 'delete groups']);
+
+        Permission::create(['name' => 'view logs']);
+
+        Permission::create(['name' => 'view scans']);
+        Permission::create(['name' => 'update scans']);
+
+        Permission::create(['name' => 'view sessions']);
+
         Permission::create(['name' => 'view users']);
         Permission::create(['name' => 'create users']);
         Permission::create(['name' => 'update users']);
         Permission::create(['name' => 'delete users']);
-
-        Permission::create(['name' => 'view scans']);
-
-        Permission::create(['name' => 'view logs']);
-
-        Permission::create(['name' => 'view sessions']);
 
         // create the admin role
         $role = Role::create(['name' => 'admin']);
