b47c3c290d9d4cf94093e200735e7fd155cf42fc
high5dev <high5dev621@hotmail.com>
2021-11-07 17:34:31 +0000

    update scans

--- app/Http/Controllers/Admin/AdminScanController.php
+++ app/Http/Controllers/Admin/AdminScanController.php
@@ -66,7 +66,7 @@ class AdminScanController extends Controller
     /**
      * admin: update a scan entry
      */
-    public function update(ScanUpdateRequest $request)
+    public function update(Request $request)
     {
         dd($request);
         if (Auth::User()->can('update scans')) {
