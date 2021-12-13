48eceefed59429fdec4996b8a349bd926329163d
high5dev <high5dev621@hotmail.com>
2021-12-13 18:56:57 +0000

    add members table

---
+++ database/migrations/2021_12_12_100100_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('username', 254);
+            $table->string('email', 254);
+            $table->string('firstip', 63)->nullable();
+            $table->datetime('zapped')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
