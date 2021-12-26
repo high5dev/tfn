e3e776d11be50564702a8ee2815d495ebf208f91
high5dev <high5dev621@hotmail.com>
2021-12-26 17:11:45 +0000

    members

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -22,7 +22,7 @@
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label" for="username">Username: </label>
                 <div class="col-sm-10">
-                    <input type="email" id="username" name="ussername" class="form-control"
+                    <input type="text" id="username" name="ussername" class="form-control"
                            value="{{ $member->username }}" readonly>
                 </div>
             </div>
@@ -30,8 +30,24 @@
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label" for="email">Email: </label>
                 <div class="col-sm-10">
-                    <input type="text" id="email" name="email"
+                    <input type="text" id="email" name="email" class="form-control"
                            value="{{ $member->email }}" readonly>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="firstip">First IP: </label>
+                <div class="col-sm-10">
+                    <input type="text" id="firstip" name="firstip" class="form-control"
+                           value="{{ $member->firstip }}" readonly>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="joined_recently">: </label>
+                <div class="col-sm-10">
+                    <input type="text" id="joined_recently" name="joined_recently" class="form-control"
+                           value="{{ $member->joined_recently ? 'YES' : 'NO' }}" readonly>
                 </div>
             </div>
 
