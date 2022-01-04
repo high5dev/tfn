85a7d74dd8711cdf5f317f5572834298ffe27680
high5dev <high5dev621@hotmail.com>
2022-01-04 20:16:17 +0000

    zap report

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -49,10 +49,8 @@
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label" for="warning_emails">Warning Emails:</label>
                 <div class="col-sm-10">
-                    <textarea id="warning_emails" name="warning_emails" class="form-control" readonly>
-                        @foreach(json_decode($report->warning_emails) as $email)
-                            {{ $email . "\n" }}
-                        @endforeach
+                    <textarea id="warning_emails" name="warning_emails" class="form-control"
+                              readonly>@foreach(json_decode($report->warning_emails) as $email){{ $email }}@endforeach
                     </textarea>
                 </div>
             </div>
