a23050d31b37ab57a7737d59d905abd1e0144988
high5dev <high5dev621@hotmail.com>
2022-01-04 20:11:18 +0000

    zap report

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -46,6 +46,14 @@
                 </div>
             </div>
 
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="warning_emails">Warning Emails:</label>
+                <div class="col-sm-10">
+                    <textarea id="warning_emails" name="warning_emails" class="form-control"
+                              readonly>{{ print_r(json_decode($report->justification)) }}</textarea>
+                </div>
+            </div>
+
             <legend>Details</legend>
 
             <div class="form-group row">
